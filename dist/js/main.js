4085c70efafcbc6dd9601e9e19a1945014b4047d
Michal Kaštan <41683416+BloodyBogan@users.noreply.github.com>
2020-05-26 21:51:01 +0200

    Chat app now works & fixed some styling issues

---
+++ dist/js/main.js
@@ -0,0 +1,54 @@
+const chatForm = document.querySelector('.chat-form');
+const chatMessages = document.querySelector('.main__messages');
+const roomName = document.querySelector('.room');
+const userList = document.querySelector('.users');
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+socket.emit('joinRoom', { username, room });
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+socket.on('message', (message) => {
+  outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  socket.emit('chatMessage', msg);
+
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message-container');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.main__messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join('')}
+    `;
+}
